429e07fe737e46a938af967e8b9e7426c11ca2ae
raikou <rai.create.program.git@gmail.com>
2025-11-08 04:25:07 +0900

    修正

--- RmFishing/Patches/OnProgressCompletePatch.cs
+++ RmFishing/Patches/OnProgressCompletePatch.cs
@@ -1,5 +1,7 @@
 ﻿using HarmonyLib;
 
+using JetBrains.Annotations;
+
 using RmModManager.UI.ModOptions;
 using RmModManager.Util;
 
@@ -8,6 +10,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Text;
+
+using UnityEngine;
 
 namespace RmModManager.Patches
 {
@@ -23,21 +27,30 @@ namespace RmModManager.Patches
 		private class BaitAndStaminaAllDatas
 		{
 			public Boolean IsPC;
-			public BaitAndStamina Start;
-			public BaitAndStamina SystemChange;//システムで変更された値
-			public BaitAndStamina Diff;
-			public BaitAndStamina Result;
+			public BaitAndStamina Start ;
+			public BaitAndStamina SystemChange = null;//システムで変更された値
+			public BaitAndStamina Diff = null;
+			public BaitAndStamina Result = null;
 
 			[Conditional("DEBUG")]
 			public void DispLog() {
 				try {
 					CommonUtil.OutputLogLinesDouble();
-					Start.LogShow("start:");
-					SystemChange.LogShow("systemChange:");
-					Diff.LogShow("diff:");
-					Result.LogShow("result:");
+					Show(Start,"start:");
+					Show(SystemChange,"systemChange:");
+					Show(Diff,"diff:");
+					Show(Result,"result:");
 					CommonUtil.OutputLogLinesDouble();
-				} catch (Exception) {
+				} catch (Exception e) {
+					CommonUtil.OutputErrorLog(e);
+				}
+			}
+
+			public void Show([CanBeNull] BaitAndStamina data, String mess) {
+				if (data == null) {
+					CommonUtil.OutputSimpleLog(":nothing");
+				} else {
+					data.LogShow(mess);
 				}
 			}
 		}
@@ -62,47 +75,68 @@ namespace RmModManager.Patches
 				ShowData(mess);
 			}
 			[Conditional("DEBUG")]
-			public void ShowData(string mess) {
+			protected void ShowData(string mess) {
 				try {
 					string message = "対象：" + _name + "餌：" + Bait.ToString() + ", スタミナ" + Stamina.ToString();
 					CommonUtil.OutputSimpleLog(mess + ", " + message);
 				} catch (Exception) {
+					CommonUtil.OutputSimpleLog(mess + ", err");
 				}
 			}
 		}
 
 		//先行処理
 		public static void Prefix(AI_Fish.ProgressFish __instance) {
-			_datasHistory.Start = new BaitAndStamina(__instance.owner);
+			try {
+				_datasHistory.Start = new BaitAndStamina(__instance.owner);
 
-			if (FishingCost == ModConfig.FishingCostEnum.DefVal) return;
+				if (FishingCost == ModConfig.FishingCostEnum.DefVal) return;
+			} catch (Exception e) {
+				CommonUtil.OutputErrorLog(e);
+			}
 		}
 
 		//後続処理
 		public static void Postfix(AI_Fish.ProgressFish __instance) {
-			_datasHistory.SystemChange = new BaitAndStamina(__instance.owner);
-			_datasHistory.SystemChange.LogShow("処理後：");
+			try {
+				_datasHistory.SystemChange = new BaitAndStamina(__instance.owner);
+				//_datasHistory.SystemChange.LogShow("処理後：");
 
-			if (FishingCost == ModConfig.FishingCostEnum.DefVal) return;
+				_datasHistory.SystemChange.LogShow("確認：1");
 
-			var chara = __instance.owner;
-			//スタミナの消費を1とする
-			chara.stamina.Set(_datasHistory.Start.Stamina);
-			chara.stamina.Mod(-1);
+				if (FishingCost == ModConfig.FishingCostEnum.DefVal) return;
+				_datasHistory.SystemChange.LogShow("確認：2");
+
+				var chara = __instance.owner;
+				//スタミナの消費を1とする
+				chara.stamina.Set(_datasHistory.Start.Stamina);
+				chara.stamina.Mod(-1);
+				_datasHistory.SystemChange.LogShow("確認：3");
 
 
-			if (FishingCost != ModConfig.FishingCostEnum.AllOne) {
-				//餌の消費を1にする
-				if (chara.IsPC) {
+				if (FishingCost != ModConfig.FishingCostEnum.AllOne) {
+					_datasHistory.SystemChange.LogShow("確認：4");
+					//餌の消費を1にする
+					if (chara.IsPC) {
+						_datasHistory.SystemChange.LogShow("確認：5");
 
-					EClass.player.eqBait.ModNum(_datasHistory.Start.Bait - 1);
+						EClass.player.eqBait.ModNum(_datasHistory.Start.Bait - 1);
+						_datasHistory.SystemChange.LogShow("確認：6");
+					}
 				}
-			}
-			_datasHistory.SystemChange = new BaitAndStamina(__instance.owner);
-			_datasHistory.SystemChange.LogShow("修正後：");
+				_datasHistory.SystemChange.LogShow("確認：7");
+				_datasHistory.SystemChange = new BaitAndStamina(__instance.owner);
+				//_datasHistory.SystemChange.LogShow("修正後：");
+				_datasHistory.SystemChange.LogShow("確認：8");
 
-			_datasHistory.DispLog();
-			//_datasHistory.DispExitParams();
+				_datasHistory.DispLog();
+				//_datasHistory.DispExitParams();
+				_datasHistory.SystemChange.LogShow("確認：9");
+			} catch (Exception e) {
+				CommonUtil.OutputErrorLog(e);
+				_datasHistory.SystemChange.LogShow("確認：error");
+				throw;
+			}
 		}
 	}
 }
